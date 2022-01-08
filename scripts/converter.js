2292e7b40e91e22631aacf52bfa70cd4d61a0c6c
Benjamin <sparty182020@gmail.com>
2022-01-08 10:14:03 -0500

    conversion script

--- scripts/converter.js
+++ scripts/converter.js
@@ -2,13 +2,12 @@
 function stbi(string) {
     const length = string.split('').length
     var i;
-    var output1 = ''
-    var output2 = ''
+    var output = ''
     for (i = 0; i < length; i++) {
-        output1 += string.charCodeAt(i).toString(2) + " "
+        output += string.charCodeAt(i).toString(2) + " "
     }
-    output1 = output1.replace(/[ \t]+$/gmi,'')
-    return output1
+    output = output.replace(/[ \t]+$/gmi,'')
+    return output
 }
 
 // Binary to string
@@ -22,4 +21,54 @@ function bits(binary) {
         output += String.fromCharCode(charcode)
     }
     return output
+}
+
+// String to hex
+function sth(string) {
+    const length = string.split('').length
+    var i;
+    var output = ''
+    for (i = 0; i < length; i++) {
+        output += string.charCodeAt(i).toString(16) + " "
+    }
+    output = output.replace(/[ \t]+$/gmi,'')
+    return output
+}
+
+// Hex to string
+function hts(hex) {
+    const length = hex.split(' ').length
+    var i;
+    var output = ''
+    for (i = 0; i < length; i++) {
+        const splith = hex.split(' ')
+        const charcode = parseInt(splith[i],16).toString(10)
+        output += String.fromCharCode(charcode)
+    }
+    return output    
+}
+
+// String to Base32
+function stb32(string) {
+    const length = string.split('').length
+    var i;
+    var output = ''
+    for (i = 0; i < length; i++) {
+        output += string.charCodeAt(i).toString(32) + " "
+    }
+    output = output.replace(/[ \t]+$/gmi,'')
+    return output
+}
+
+// Base32 to string
+function b32ts(b32) {
+    const length = b32.split(' ').length
+    var i;
+    var output = ''
+    for (i = 0; i < length; i++) {
+        const splitb32 = b32.split(' ')
+        const charcode = parseInt(splitb32[i],32).toString(10)
+        output += String.fromCharCode(charcode)
+    }
+    return output    
 }
