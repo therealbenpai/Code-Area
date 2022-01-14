3b214ca3d1ef90dbe032f13ab9d4dc36ace428c2
sparty182020 <sparty182020@gmail.com>
2022-01-14 08:27:34 -0500

    mcts

--- scripts/converter.js
+++ scripts/converter.js
@@ -51,10 +51,10 @@ const stmca = {
 function stmc(string) {
     const scRegex = /\w/
     const ncRegex = /\d/
-    const splitstr = string.split('')
+    const splitstr = string.toLowerCase().split('')
     const length = splitstr.length
     let i;
-    let output
+    var output = ''
     for (i=0;i<length;i++) {
         // Tests if its a "non-word character"
         if (scRegex.test(splitstr[i]) == false) {
@@ -97,52 +97,152 @@ function stmc(string) {
             }
         } else {
             if (splitstr[i] == "a") {
-
+                output += `${stmca.a} `
             } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
-            } else if (splitstr[i] == "b") {
-
+                output += `${stmca.b} `
+            } else if (splitstr[i] == "c") {
+                output += `${stmca.c} `
+            } else if (splitstr[i] == "d") {
+                output += `${stmca.d} `
+            } else if (splitstr[i] == "e") {
+                output += `${stmca.e} `
+            } else if (splitstr[i] == "f") {
+                output += `${stmca.f} `
+            } else if (splitstr[i] == "g") {
+                output += `${stmca.g} `
+            } else if (splitstr[i] == "h") {
+                output += `${stmca.h} `
+            } else if (splitstr[i] == "i") {
+                output += `${stmca.i} `
+            } else if (splitstr[i] == "j") {
+                output += `${stmca.j} `
+            } else if (splitstr[i] == "k") {
+                output += `${stmca.k} `
+            } else if (splitstr[i] == "l") {
+                output += `${stmca.l} `
+            } else if (splitstr[i] == "m") {
+                output += `${stmca.m} `
+            } else if (splitstr[i] == "n") {
+                output += `${stmca.n} `
+            } else if (splitstr[i] == "o") {
+                output += `${stmca.o} `
+            } else if (splitstr[i] == "p") {
+                output += `${stmca.p} `
+            } else if (splitstr[i] == "q") {
+                output += `${stmca.q} `
+            } else if (splitstr[i] == "r") {
+                output += `${stmca.r} `
+            } else if (splitstr[i] == "s") {
+                output += `${stmca.s} `
+            } else if (splitstr[i] == "t") {
+                output += `${stmca.t} `
+            } else if (splitstr[i] == "u") {
+                output += `${stmca.u} `
+            } else if (splitstr[i] == "v") {
+                output += `${stmca.v} `
+            } else if (splitstr[i] == "w") {
+                output += `${stmca.w} `
+            } else if (splitstr[i] == "x") {
+                output += `${stmca.x} `
+            } else if (splitstr[i] == "y") {
+                output += `${stmca.y} `
+            } else if (splitstr[i] == "z") {
+                output += `${stmca.z} `
             }
+        }
+    }
+    return output.replace(/^[ \t]+|[ \t]+$/gmi,'')
+}
+
+// Morse Code to String
+function mcts(morseCode) {
+    const splitMC = morseCode.split(' ')
+    const length = splitMC.length
+    let i;
+    let output = ''
+    for (i=0;i<length;i++) {
+        if (splitMC[i] == stmca.a) {
+            output += `a`
+        } else if (splitMC[i] == stmca.b) {
+            output += `a`
+        } else if (splitMC[i] == stmca.c) {
+            output += `a`
+        } else if (splitMC[i] == stmca.d) {
+            output += `a`
+        } else if (splitMC[i] == stmca.e) {
+            output += `a`
+        } else if (splitMC[i] == stmca.f) {
+            output += `a`
+        } else if (splitMC[i] == stmca.g) {
+            output += `a`
+        } else if (splitMC[i] == stmca.h) {
+            output += `a`
+        } else if (splitMC[i] == stmca.i) {
+            output += `a`
+        } else if (splitMC[i] == stmca.j) {
+            output += `a`
+        } else if (splitMC[i] == stmca.k) {
+            output += `a`
+        } else if (splitMC[i] == stmca.l) {
+            output += `a`
+        } else if (splitMC[i] == stmca.m) {
+            output += `a`
+        } else if (splitMC[i] == stmca.n) {
+            output += `a`
+        } else if (splitMC[i] == stmca.o) {
+            output += `a`
+        } else if (splitMC[i] == stmca.p) {
+            output += `a`
+        } else if (splitMC[i] == stmca.q) {
+            output += `a`
+        } else if (splitMC[i] == stmca.r) {
+            output += `a`
+        } else if (splitMC[i] == stmca.s) {
+            output += `a`
+        } else if (splitMC[i] == stmca.t) {
+            output += `a`
+        } else if (splitMC[i] == stmca.u) {
+            output += `a`
+        } else if (splitMC[i] == stmca.v) {
+            output += `a`
+        } else if (splitMC[i] == stmca.w) {
+            output += `a`
+        } else if (splitMC[i] == stmca.x) {
+            output += `a`
+        } else if (splitMC[i] == stmca.y) {
+            output += `a`
+        } else if (splitMC[i] == stmca.z) {
+            output += `a`
+        } else if (splitMC[i] == stmca.specials.space) {
+            output += `a`
+        } else if (splitMC[i] == stmca.specials.period) {
+            output += `a`
+        } else if (splitMC[i] == stmca.specials.comma) {
+            output += `a`
+        } else if (splitMC[i] == stmca.specials.dash) {
+            output += `a`
+        } else if (splitMC[i] == stmca.specials.slash) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][0]) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][1]) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][2]) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][3]) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][4]) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][5]) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][6]) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][7]) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][8]) {
+            output += `a`
+        } else if (splitMC[i] == stmca.number[0][9]) {
+            output += `a`
         }
     }
 }
