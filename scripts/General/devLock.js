fa78bd1811b6c121425c2da91f19040bf2e3b3a3
sparty182020 <sparty182020@gmail.com>
2022-01-15 19:18:46 -0500

    Dev Locking

---
+++ scripts/General/devLock.js
@@ -0,0 +1,21 @@
+function passwd() {
+    const sul = localStorage.getItem('drm')
+    if (sul == true) {
+        document.body.style.visibility = 'visible'
+    } else {
+        const passwd = prompt('Developer Password:')
+        const epsw = atob('ZGV2cHN3')
+        if (passwd == espw) {
+            const rm = confirm('Remeber Login (\'Ok\' = True, \'Cancel\' = False)')
+            if (rm == true) {
+                localStorage.setItem('drm', 'true')
+            }
+            document.body.style.visibility = 'visible'
+        } else {
+            alert('Bad Password')
+            console.error('Bad Password entered. password: ' + passwd)
+        }
+    }
+}
+
+document.onload(() => passwd())
