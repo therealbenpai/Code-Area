aa1fb00e7d76caadaf4ed66ea4a8a1683435bb55
Benjamin <sparty182020@gmail.com>
2022-01-24 19:37:01 -0500

    run

--- scripts/Other/testing.js
+++ scripts/Other/testing.js
@@ -1,5 +1,53 @@
-const keygen = function () {
+const l4KeyGen = function () {
     return Math.ceil(Math.random() * 8) * 1000 + Math.ceil(Math.random() * 8) * 100 + Math.ceil(Math.random() * 8) * 10 + Math.ceil(Math.random() * 8)
 }
 
-function
+function FDIDG(id) {
+    let ev;
+    if (id != null || id != undefined) {
+        if (String(parseInt(id,10)).length != 4) {
+            return console.error('ID needs to be 4 digits long')
+        } else {
+            ev = id
+        }
+    } else {
+        ev = l4KeyGen()
+    }
+    for (let i = 0; i < 10000; i++) {
+        let v = l4KeyGen()
+        if (v == ev) {
+            console.log(`id of ${ev} was found on iteration ${i}`)
+            break;
+        } else {
+            console.log(v)
+        }
+    }
+}
+
+const collatz = function(sn,mi) {
+    if (sn <= 0) {
+        return console.error('Starting Number must be greater than 0')
+    }
+    let fmi = 0
+    let it = 0
+    let f;
+    if (mi != null || mi != undefined) {
+        fmi = mi;
+    } else {
+        // ?Defult Max Value
+        fmi = 1000;
+    }
+    for (let i = 0; i < fmi; i++) {
+        if (sn % 2 == 1) {
+            sn = (sn * 3) + 1;
+        } else {
+            sn = sn / 2;
+        }
+        if (sn == 8) {
+            it = i;
+            f = true;
+            break;
+        }
+    }
+    return console.log(`%cRepeating starts at ${it} iterations`,'color: lightblue;')
+}
