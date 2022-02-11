dca112a0fa8a2bd8af0222f45eed5bdf8987d776
Benjamin <sparty182020@gmail.com>
2022-02-11 15:27:28 -0500

    update

--- scripts/General/devLock.js
+++ scripts/General/devLock.js
@@ -5,15 +5,19 @@ function passwd() {
     } else {
         const passwd = prompt('Developer Password:')
         const epsw = atob('ZGV2cHN3')
-        if (passwd == epsw) {
-            const rm = confirm('Remeber Login (\'Ok\' = True, \'Cancel\' = False)')
-            if (rm == true) {
-                localStorage.setItem('drm', 'true')
+        try {
+            if (passwd == epsw) {
+                const rm = confirm('Remeber Login (\'Ok\' = True, \'Cancel\' = False)')
+                if (rm == true) {
+                    localStorage.setItem('drm', 'true')
+                }
+                document.body.style.visibility = 'visible'
+            } else {
+                alert('Bad Password')
+                new Error('Bad Passkey entered. password: ' + passwd)
             }
-            document.body.style.visibility = 'visible'
-        } else {
-            alert('Bad Password')
-            console.error('Bad Password entered. password: ' + passwd)
+        } catch (error) {
+            console.error(error)
         }
     }
 }
