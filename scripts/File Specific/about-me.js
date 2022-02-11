074393874af66559f81e25ac857fa24d24a4974b
Benjamin <sparty182020@gmail.com>
2022-02-11 16:37:13 -0500

    try again

--- scripts/File Specific/about-me.js
+++ scripts/File Specific/about-me.js
@@ -1,9 +1,9 @@
 // About Me animation
 var i = 0;
 var speed = 50;
+const text = 'About Me'
 
 function animation() {
-    const text = 'About Me'
     const element = document.getElementById('aboutme')
     if (i < text.length) {
         element.innerText += text.charAt(i)
