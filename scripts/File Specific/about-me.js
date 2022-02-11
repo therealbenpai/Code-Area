604f47ddbc3171030ee07ec4769225ad49dbe5be
Benjamin <sparty182020@gmail.com>
2022-02-11 16:49:34 -0500

    fix

--- scripts/File Specific/about-me.js
+++ scripts/File Specific/about-me.js
@@ -5,11 +5,22 @@ const text = 'About Me'
 
 function animation() {
     const element = document.getElementById('aboutme')
+    const splitStr = text.split('')
     if (i < text.length) {
-        const ct = element.innerText
-        const nt = ct + text.charAt(i)
-        element.innerText = nt
-        i++;
-        setTimeout(animation, speed);
+        if (i == 6) {
+            const ct = element.innerText
+            const nt = ct + " " + splitStr[i]
+            element.innerText = nt
+            i++
+            setTimeout(animation, speed);
+        } else {
+            const ct = element.innerText
+            const nt = ct + splitStr[i]
+            element.innerText = nt
+            i++;
+            setTimeout(animation, speed);
+        }
     }
 }
+
+setTimeout(animation, 1000)
