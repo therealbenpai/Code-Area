3c4db73f3089a9d8bbd1b4ac02ffb04219b04aee
Benjamin <sparty182020@gmail.com>
2022-02-13 16:31:24 -0500

    push 1

--- scripts/File Specific/toolbox.js
+++ scripts/File Specific/toolbox.js
@@ -1,27 +1,33 @@
-// Vars
-const w1 = ""
-const w2 = ""
-const w3 = ""
-const w4 = ""
-const w5 = ""
-
 // Functions
+
+// Classes
 class Util {
     constructor() {
-        this.currentText = navigator.clipboard.readText().then(data => {return data}),
-        this.current = navigator.clipboard.read().then(data => {return data;})
         this.time = this.getTime()
         this.date = this.getDate()
         this.fullDate = this.getFullDate()
     }
     getTime() {
         const datemod = new Date()
-        const time = datemod.toLocaleTimeString('en-US', { timeZone: 'EST' })
+        const time = datemod.toLocaleTimeString(
+            'en-US',
+            {
+                timeZone: 'EST'
+            }
+        )
         return time
     }
     getDate() {
         const datemod = new Date();
-        const date = datemod.toLocaleDateString('en-US', { timeZone: 'EST', year: "2-digit", month: "2-digit", day: "2-digit" })
+        const date = datemod.toLocaleDateString(
+            'en-US',
+            {
+                timeZone: 'EST',
+                year: "2-digit",
+                month: "2-digit",
+                day: "2-digit"
+            }
+        )
         return date;
     }
     getFullDate() {
@@ -29,12 +35,20 @@ class Util {
         const time = this.getTime()
         return `${date} ${time}`
     }
-    readClipboard() {
-        return navigator.clipboard.read()
-    }
-    writeClipboard(information) {
-        return navigator.clipboard.write(information)
+    writeClipboard(index) {
+        const writeables = [
+            "",
+            "",
+            "",
+            "",
+            ""
+        ]
+        if (index > writeables.length) {
+            return new Error("Failed To Write: Invalid Index")
+        } else {
+            return navigator.clipboard.writeText(writeables[index - 1])
+        }
     }
 }
 
-const x = new Util()
+const domUtil = new Util()
